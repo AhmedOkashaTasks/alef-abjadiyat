4397d4664de91ff60a16a51755fdaf8f70a9eb71
ahmadrokasha <ahmad.r.okasha@gmail.com>
2022-07-03 00:18:41 +0400

    add div

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,8 @@ const App: React.FC = () =>{
   return (
     <div className="App">
 <p>alef task</p>
-<span>alef task</span>
-<h1>alef task</h1>
-<h2>alef task</h2>
-<h3>alef task</h3>
-<h4>alef task</h4>
+<div>alef task</div>
+
 
       <h2>hi</h2>
           <Home/>
